a53ba16b0b10a2c899cb461d906b8334d956ab97
thebhut <bhutjaydip1554@gmail.com>
2026-01-17 14:11:38 +0530

    feat: Add initial portfolio website home page and enforce HTTPS in production.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css?v=2') }}">
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div id="root">
